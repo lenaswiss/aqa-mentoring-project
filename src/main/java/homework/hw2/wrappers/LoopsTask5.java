929576a749461190584b9305ffea9f0cd418e640
lena <cherchenko.olena@airslate.com>
2023-07-20 21:50:21 +0200

    Home works

--- src/main/java/homework/hw2/wrappers/LoopsTask5.java
+++ src/main/java/homework/hw2/wrappers/LoopsTask5.java
@@ -7,9 +7,11 @@ import java.util.Collections;
 public class LoopsTask5 {
 
     public static void main(String[] args) {
-//        printRectangle(4);
-//        printTriangleLeft(5);
-//        printTriangleRight(5);
+        printRectangle(4);
+        printTriangleLeft(5);
+        printTriangleRight(5);
+        printAsterisksTriangle(5);
+        printNumbersTriangle(5);
         printNumbersTriangle2(5);
     }
 
@@ -43,6 +45,20 @@ public class LoopsTask5 {
         }
     }
 
+    public static void printAsterisksTriangle(int height) {
+        int counter = 1;
+        for (int i = 0; i < height; i++) {
+            for (int j = 1; j <= height + i; j++) {
+                if (j == height || j >= height - i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            counter++;
+        }
+    }
     public static void printNumbersTriangle(int height) {
         int counter = 1;
         for (int i = 0; i < height; i++) {
@@ -59,31 +75,22 @@ public class LoopsTask5 {
     }
 
     public static void printNumbersTriangle2(int height) {
-        int counter = 1;
         for (int i = 0; i < height; i++) {
-
-            for (int j = 1; j <= height; j++) {
-                if (j >= height - i) {
-                    int temp = counter;
-                    for (int k = height; k <= height; k++) {
-                        System.out.print(temp);
-                        temp--;
-                    }
-                    for (int k = height; k <= height; k++) {
-                        System.out.print(temp);
-                        temp++;
+            int num = 1 + i;
+            for (int j = 0; j < height * 2; j++) {
+                if (j >= (height - i) && j <= (height + i)) {
+                    System.out.print(num);
+                    if (j < height) {
+                        num--;
+                    } else {
+                        num++;
                     }
                 } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
-            counter++;
         }
     }
 
-    public int[] makeMiddle(int[] nums) {
-        int[] array = new int[] {nums[nums.length/2], nums[nums.length/2+1]};
-        return array;
-    }
 }
