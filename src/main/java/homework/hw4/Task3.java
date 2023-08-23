d77b90adc01d78e01baff120c8679b1878abc5d7
lena <cherchenko.olena@airslate.com>
2023-08-23 08:45:21 +0200

    1. Написать программу которая обьявляет поля примитивных типов и основных ссылочных типов (Object, String, Integer, List) без инициализации и выводит на печать значения этих полей 2. Написать программу которая демонстрирует затемнение полей. Иерархия классов должна состоять минимум из трех классов. Программа выводи на печать из дочернего класса затемняющего и все затемненные поля родителей 3. Написать программу которая демонстрирует все возможные типы инициализации  и  переменных

--- src/main/java/homework/hw4/Task3.java
+++ src/main/java/homework/hw4/Task3.java
@@ -1,2 +1,40 @@
-package homework.hw4;public class Task3 {
+package homework.hw4;
+
+public class Task3 {
+    /**
+     * 3. Написать программу которая демонстрирует все возможные типы инициализации `final` и `static final`
+     *  переменных
+     */
+    final int A = 1;
+    final int B;{
+        B = 2;
+    }
+    final int C;
+    Task3(){
+        C = 3;
+    }
+
+    public static void main(String[] args) {
+        Task3 t = new Task3();
+        System.out.println("A = " + t.A  + ", B = " + t.B + ", C = " + t.C);
+    }
+}
+
+class FinalVariables {
+    final static int FINAL_STATIC;
+    final int FINAL_NOT_STATIC ;
+
+    FinalVariables(int final_not_static, int final_static) {
+        FINAL_NOT_STATIC = final_not_static;
+    }
+
+    static {
+        FINAL_STATIC = 1;
+//        FINAL_NOT_STATIC = 23; //в блоке статической инициализации
+        final int FINAL_NOT_STATIC_BLOCK = 15 ;
+    }
+
+//    public void changeFinal(){
+//        FINAL_NOT_STATIC = 4;
+//    }
 }
